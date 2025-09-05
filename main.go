89e24590fd7a81a6180118724a9711339ffbe4ef
Dustin Alandzes <dustin.alandzes@hp.com>
2025-09-05 11:40:50 -0500

    finish delete endpoint

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strconv"
 
 	"github.com/backend-project/auth"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -145,109 +141,6 @@ func (app *App) logout(c *gin.Context) {
 	c.SetCookie("token", "", -1, "/", "localhost", false, true)
 }
 
-// Paginate https://gorm.io/docs/scopes.html#Pagination
-func Paginate(r *http.Request) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		q := r.URL.Query()
-		page, _ := strconv.Atoi(q.Get("page"))
-		if page <= 0 {
-			page = 1
-		}
-
-		pageSize, _ := strconv.Atoi(q.Get("page_size"))
-		switch {
-		case pageSize > 100:
-			pageSize = 100
-		case pageSize <= 0:
-			pageSize = 10
-		}
-
-		offset := (page - 1) * pageSize
-		return db.Offset(offset).Limit(pageSize)
-	}
-}
-
-func (app *App) getFiles(c *gin.Context) {
-	var files []File
-	result := app.db.Scopes(Paginate(c.Request)).Find(&files)
-	if result.Error != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error.Error()})
-	}
-	c.JSON(http.StatusOK, files)
-	return
-}
-
-func (app *App) doesFileNameExist(ctx *gin.Context, fileName string) bool {
-	_, err := gorm.G[File](app.db).Where("file_path LIKE ?", fileName).First(ctx)
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false
-		}
-		panic(err)
-	} else {
-		return true
-	}
-}
-
-func (app *App) generateUniqueFileName(ctx *gin.Context) string {
-	uniqueName := uuid.New().String()
-
-	// Timestamp?
-
-	// I'm not sure if this is necessary, but I'm not sure if the uuid is really guaranteed to be unique
-	for app.doesFileNameExist(ctx, uniqueName) {
-		uniqueName = uuid.New().String()
-	}
-
-	return uniqueName
-}
-
-func (app *App) createFile(c *gin.Context) {
-
-	uploadedFile, err := c.FormFile("file")
-	fileName := c.PostForm("name")
-	fileDescription := c.DefaultPostForm("description", "")
-	//tags := c.DefaultPostForm("tags", "[]")
-	//tagStructs := []Tag{}
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	uploadPath := os.Getenv("UPLOAD_PATH")
-	if uploadPath == "" {
-		uploadPath = "./files"
-	}
-
-	uniqueFileName := filepath.Base(app.generateUniqueFileName(c))
-
-	err = c.SaveUploadedFile(uploadedFile, fmt.Sprintf("%s/%s", uploadPath, uniqueFileName))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	file := File{Name: fileName, Description: fileDescription, FilePath: uniqueFileName, Tags: []Tag{}}
-	err = gorm.G[File](app.db).Create(
-		c,
-		&file,
-	)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// I'm querying the database here to get the updatedAt, createdAt, timestamps
-	fileFromDatabase, err := gorm.G[File](app.db).Where(&File{ID: file.ID}).First(c)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, fileFromDatabase)
-}
-
 func (app *App) setupRouter() *gin.Engine {
 	fmt.Println("Setting up router...")
 
@@ -263,9 +156,11 @@ func (app *App) setupRouter() *gin.Engine {
 	router.POST("/login", app.login)
 	router.GET("/logout", app.logout)
 
-	// files crud
+	// files
+	router.GET("/files/:id", app.getFile)
 	router.GET("/files", app.getFiles)
 	router.POST("/files", app.createFile)
+	router.DELETE("/files/:id", app.deleteFile)
 	return router
 }
 
--- main_test.go
+++ main_test.go
@@ -4,13 +4,17 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 )
@@ -247,4 +251,225 @@ func TestGetFiles(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 	expectedJson, err = json.Marshal([]File{expected})
 	assert.Equal(t, string(expectedJson), w.Body.String())
+}
+
+func TestGetFile(t *testing.T) {
+	defer cleanUp()
+
+	err := os.Setenv("ENVIRONMENT", "TEST")
+	if err != nil {
+		panic(err)
+	}
+	db := setupDatabase()
+	app := App{db: db}
+	router := app.setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/files", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "[]", w.Body.String())
+
+	// upload a file
+	// Create a dummy file for testing
+	dummyFileContent := []byte("This is a test file content.")
+	dummyFileName := "testfile.txt"
+	err = os.WriteFile(dummyFileName, dummyFileContent, 0644)
+	assert.NoError(t, err)
+	defer func(name string) {
+		err := os.Remove(name)
+		if err != nil {
+			panic(err)
+		}
+	}(dummyFileName) // Clean up the dummy file
+
+	// Create a new multipart writer
+	fileBody := new(bytes.Buffer)
+	writer := multipart.NewWriter(fileBody)
+
+	// Create a form file field
+	file, err := os.Open(dummyFileName)
+	assert.NoError(t, err)
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	_, err = writer.CreateFormField("name")
+	if err != nil {
+		return
+	}
+	err = writer.WriteField("name", "test-filename")
+	if err != nil {
+		return
+	}
+
+	_, err = writer.CreateFormField("description")
+	if err != nil {
+		return
+	}
+	err = writer.WriteField("description", "this is a test file")
+	if err != nil {
+		return
+	}
+
+	_, err = writer.CreateFormField("tags")
+	if err != nil {
+		return
+	}
+	err = writer.WriteField("description", "[]")
+	if err != nil {
+		return
+	}
+
+	part, err := writer.CreateFormFile("file", dummyFileName) // "myFile" must match the name in the handler
+
+	assert.NoError(t, err)
+	_, err = io.Copy(part, file)
+	assert.NoError(t, err)
+
+	// Close the multipart writer
+	err = writer.Close()
+	assert.NoError(t, err)
+
+	w = httptest.NewRecorder()
+
+	req, _ = http.NewRequest("POST", "/files", fileBody)
+	req.Header.Set("Content-Type", writer.FormDataContentType()) // Set the correct Content-Type header
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	expected, err := gorm.G[File](app.db).Order("created_at desc").First(context.TODO())
+	if err != nil {
+		panic(err)
+	}
+	expectedJson, err := json.Marshal(expected)
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, string(expectedJson), w.Body.String())
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", fmt.Sprintf("/files/%s", strconv.Itoa(int(expected.ID))), nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string(expectedJson), w.Body.String())
+}
+
+func TestDeleteFile(t *testing.T) {
+	defer cleanUp()
+
+	err := os.Setenv("ENVIRONMENT", "TEST")
+	if err != nil {
+		panic(err)
+	}
+	db := setupDatabase()
+	app := App{db: db}
+	router := app.setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/files", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "[]", w.Body.String())
+
+	// upload a file
+	// Create a dummy file for testing
+	dummyFileContent := []byte("This is a test file content.")
+	dummyFileName := "testfile.txt"
+	err = os.WriteFile(dummyFileName, dummyFileContent, 0644)
+	assert.NoError(t, err)
+	defer func(name string) {
+		err := os.Remove(name)
+		if err != nil {
+			panic(err)
+		}
+	}(dummyFileName) // Clean up the dummy file
+
+	// Create a new multipart writer
+	fileBody := new(bytes.Buffer)
+	writer := multipart.NewWriter(fileBody)
+
+	// Create a form file field
+	file, err := os.Open(dummyFileName)
+	assert.NoError(t, err)
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	_, err = writer.CreateFormField("name")
+	if err != nil {
+		return
+	}
+	err = writer.WriteField("name", "test-filename")
+	if err != nil {
+		return
+	}
+
+	_, err = writer.CreateFormField("description")
+	if err != nil {
+		return
+	}
+	err = writer.WriteField("description", "this is a test file")
+	if err != nil {
+		return
+	}
+
+	_, err = writer.CreateFormField("tags")
+	if err != nil {
+		return
+	}
+	err = writer.WriteField("description", "[]")
+	if err != nil {
+		return
+	}
+
+	part, err := writer.CreateFormFile("file", dummyFileName) // "myFile" must match the name in the handler
+
+	assert.NoError(t, err)
+	_, err = io.Copy(part, file)
+	assert.NoError(t, err)
+
+	// Close the multipart writer
+	err = writer.Close()
+	assert.NoError(t, err)
+
+	w = httptest.NewRecorder()
+
+	req, _ = http.NewRequest("POST", "/files", fileBody)
+	req.Header.Set("Content-Type", writer.FormDataContentType()) // Set the correct Content-Type header
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	expected, err := gorm.G[File](app.db).Order("created_at desc").First(context.TODO())
+	if err != nil {
+		panic(err)
+	}
+	expectedJson, err := json.Marshal(expected)
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, string(expectedJson), w.Body.String())
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("DELETE", fmt.Sprintf("/files/%s", strconv.Itoa(int(expected.ID))), nil)
+	router.ServeHTTP(w, req)
+
+	expectedJson, err = json.Marshal(gin.H{"success": true})
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string(expectedJson), w.Body.String())
+
+	_, err = gorm.G[File](app.db).Where("id = ?", expected.ID).First(context.TODO())
+	assert.True(t, errors.Is(err, gorm.ErrRecordNotFound))
 }
